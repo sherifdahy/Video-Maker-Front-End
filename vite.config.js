327da5faaeae400e0704df30b87f0c0e2c831c67
Sherif Dahy <shrifm2017@gmail.com>
2026-04-16 15:39:01 +0200

    Reinstall dependencies - fix vite permission issue

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(
-      process.env.VITE_API_URL || 'http://localhost:3001'
-    )
-  },
   server: {
-    port: 5173,
-    proxy: {
-      '/api': process.env.VITE_API_URL || 'http://localhost:3001',
-      '/output': process.env.VITE_API_URL || 'http://localhost:3001',
-    }
+    port: 5173
   }
 })
